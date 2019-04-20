9cbdf9422281c10220b49e5206bdcb17b207bb35
alvi032 <talha.6862@gmail.com>
2019-04-20 22:16:24 +0500

    added loader

---
+++ main.js
@@ -0,0 +1,6 @@
+setTimeout( function(){
+    $('#loader').css({'display' : 'none'});
+    // $('#main-content').css({'padding-top' : '65px'});
+    $('#main-content').css({'display' : 'block'});
+
+}  , 5000 );
