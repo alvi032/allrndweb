f615c0d0b337dfcaac73130a625b5f0e627f04ff
alvi032 <talha.6862@gmail.com>
2019-04-20 22:30:44 +0500

    fixed padding top issue

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-setTimeout( function(){
+/*setTimeout( function(){
     $('#loader').css({'display' : 'none'});
     // $('#main-content').css({'padding-top' : '65px'});
     $('#main-content').css({'display' : 'block'});
 
-}  , 5000 );
+}  , 5000 );*/
+
+var width = $(window).width();
+if (width <= 500){
+    setTimeout( function(){
+        $('#loader').css({'display' : 'none'});
+        $('body').css({'padding-top' : '80px'});
+        $('#main-content').css({'display' : 'block'});
+
+    }  , 5000 );
+}
+else{
+    $('#main-content').css({'padding-top' : '100px'});
+}
