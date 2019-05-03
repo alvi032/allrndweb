ae805df4aeb22fd2c3a5162ab6746d9105c27f64
alvi032 <talha.6862@gmail.com>
2019-05-03 19:07:06 +0500

    fixed portfolio responsiveness

--- portfolio.js
+++ portfolio.js
@@ -2,25 +2,21 @@ $("#all").click(function(){
     $(".poster").css("display", "block");
     $(".brand").css("display", "block");
     $(".videos").css("display", "block");
-    $("#posters").css("grid-template-columns", "repeat(4, 1fr)");
 });
 $("#adverts").click(function(){
     $(".poster").css("display", "block");
     $(".brand").css("display", "none");
     $(".videos").css("display", "none");
-    $("#posters").css("grid-template-columns", "repeat(4, 1fr)");
 });
 $("#branding").click(function(){
     $(".poster").css("display", "none");
     $(".brand").css("display", "block");
     $(".videos").css("display", "none");
-    $("#posters").css("grid-template-columns", "repeat(3, 1fr)");
 });
 $("#videos").click(function(){
     $(".poster").css("display", "none");
     $(".brand").css("display", "none");
     $(".videos").css("display", "block");
-    $("#posters").css("grid-template-columns", "repeat(4, 1fr)");
 });
 
 var modal = document.getElementById("bg-fs");
